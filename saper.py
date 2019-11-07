34993ac38b0f8189dab91ff07ca1cfb44370c125
Konstantin <kasyanov_k@list.ru>
2019-11-07 10:27:24 +0300

    added filling saper field

--- saper.py
+++ saper.py
@@ -22,12 +22,38 @@ for i in range(len(field)):
         s_result += "%3i" % field[i][j]
     s_result += '\n'
 print(s_result)
+print("\n")
 
-extended_field = [[field[i][j] for i in range(0, field_x + 2) if 1 < i < field_x + 1] for j in range(0, field_y + 2) if
-                  1 < j < field_y + 1]
+# для эффективного единообразного заполнения поля предлагается расширить поле со всех сторон нулями
+extended_field = [[0 for i in range(len(field[0]) + 2)] for j in range(len(field) + 2)]
+# extended_field[1:len(field) + 1][1:len(field[0]) + 1] = field слайсы слайсов не работают
+for i in range(1, len(field) + 1):
+    extended_field[i][1:len(field[0]) + 1] = field[i - 1][0:len(field[0])]
+    # for j in range(1, field_x + 1):
+    #     extended_field[i][j] = field[i-1][j-1]
+for i in range(1, len(extended_field) - 1):
+    for j in range(1, len(extended_field[i]) - 1):
+        if extended_field[i][j] != -1:
+            count_bomb_by_cell = 0
+            for m in range(i - 1, i + 2):
+                for n in range(j - 1, j + 2):
+                    if extended_field[m][n] == -1:
+                        count_bomb_by_cell += 1
+            extended_field[i][j] = count_bomb_by_cell
+# field = extended_field[1:len(extended_field) - 1][1:len(extended_field[0]) - 1] работает не так как надо режет почему то сверху а не слева и справа
+for i in range(len(field)):
+    field[i] = extended_field[i + 1][1:len(extended_field[i + 1]) - 1]
 
+s_result = ""
 for i in range(len(extended_field)):
     for j in range(len(extended_field[i])):
         s_result += "%3i" % extended_field[i][j]
+    s_result += '\n'
+print(s_result)
+
+s_result = ""
+for i in range(len(field)):
+    for j in range(len(field[i])):
+        s_result += "%3i" % field[i][j]
     s_result += '\n'
 print(s_result)
