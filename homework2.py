10f54b96e9dddfc165d47a6cbe0deb1dbdf8e46e
Konstantin <kasyanov_k@list.ru>
2019-11-07 10:37:23 +0300

    added parse float from some string

---
+++ homework2.py
@@ -0,0 +1,36 @@
+# task1 print string with deleted repeating
+print("#task1 print string with deleted repeating")
+s = "aaaabbbbcccddeeeeeeffff"
+s = s + "\n"
+new_s = ""
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        new_s = new_s + s[i]
+print(new_s)
+
+# task2 calculate the numbers of entry upper and lower characters
+print("#task2 calculate the numbers of entry upper and lower characters")
+s = "AAAAAvvvvvvBBBBBssssssTTTTTTSSSSS"
+count_lower = 0
+count_upper = 0
+for c in s:
+    if c.isupper():
+        count_upper += 1
+    elif c.islower():
+        count_lower += 1
+print("Count lower =" + str(count_lower) + " count upper =" + str(count_upper))
+
+# task3 print all numbers
+print("#task3 print all numbers from string")
+s = "166666bla-bla-bla 1480000000 bla-blabla 40000 bla-bla-bla50000dfgdfgdfgd464564.5645"
+s = s + "\n"
+count_digit = 0
+start_digit = 0
+for i in range(len(s)):
+    if s[i].isdigit() or (s[i] == '.' and count_digit > 0):
+        if count_digit == 0:
+            start_digit = i
+        count_digit += 1
+    elif count_digit != 0:
+        print(float(s[start_digit:start_digit + count_digit]))
+        count_digit = 0
